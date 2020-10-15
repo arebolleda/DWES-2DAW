fedca8c8b22510aa597853799e51e8cd22ad200c
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2020-10-15 09:26:08 +0200

    Update ej5a.php

--- PHP/ej5a.php
+++ PHP/ej5a.php
@@ -11,7 +11,7 @@
             $array2 = array("Sistemas Informáticos","FOL","Mecanizado");
             $array3 = array("Desarrollo Web ES","Desarrollo Web EC","Despliegue","Desarrollo Interfaces", "Inglés");
 
-
+             /*ALFONSO:<<<< Se deben utilizar bucles para hacer la unión dinámica -> si los arrays tuvieran miles de elementos como lo harías?? */
             // APARTADO A
             $array4 = array($array1[0],$array1[1],$array1[2],$array2[0],$array2[1],$array2[2],$array3[0],$array3[1],$array3[2],$array3[3],$array3[4]);
 
@@ -20,7 +20,7 @@
             }
             echo "<br><br>";
 
-
+             /*ALFONSO:<<<< Muy bien resuelto*/
             // APARTADO B
             $mergedarray = array_merge($array1,$array2,$array3);
 
@@ -31,6 +31,7 @@
 
 
             // APARTADO C
+            /*ALFONSO:<<<< Se deben utilizar bucles para hacer el apartado*/
             $pushedarray = array();
             array_push($pushedarray,$array1[0],$array1[1],$array1[2],$array2[0],$array2[1],$array2[2],$array3[0],$array3[1],$array3[2],$array3[3],$array3[4]);
 
