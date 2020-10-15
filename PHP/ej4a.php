54cb6e902ced6bb8be6155918fe4688863c7729e
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2020-10-15 09:23:29 +0200

    Update ej4a.php

--- PHP/ej4a.php
+++ PHP/ej4a.php
@@ -6,7 +6,7 @@
     <body>
         <?php  
             // PEDRO FERNÁNDEZ GARCÍA
-
+      /*ALFONSO:<<<< El contenido del array lo tienes que generar de manera dinámica, no de manera estática */
             $impares=array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);   
 
             $arraybinarios = array_reverse($impares);
