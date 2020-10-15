d94a947d7264eafc7ec952fa9711fc93449b2d4f
Alfonso Rebolleda Sánchez <47109499+arebolleda@users.noreply.github.com>
2020-10-15 09:28:18 +0200

    Update ej8a.php

--- PHP/ej8a.php
+++ PHP/ej8a.php
@@ -10,11 +10,12 @@
             $arrayasociativo = array ("Pedro"=>"9","Carlos"=>"7","Alex"=>"7","Marco"=>"6","Rubén"=>"8");
 
             // APARTADO A
+            /*ALFONSO:<<<< Se muestra la Nota pero no el nombre del alumno*/
             echo "Máximo: ".max($arrayasociativo)."<br>";
-            
+              /*ALFONSO:<<<< Se muestra la Nota pero no el nombre del alumno*/
             // APARTADO B
             echo "Minimo: ".min($arrayasociativo)."<br>";
-            
+              /*ALFONSO:<<<< OK */
             // APARTADO C
             echo "Media: ".array_sum($arrayasociativo) / count($arrayasociativo)."<br>";
             ?>
